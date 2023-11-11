e4797d15966f1847af60bc349870fcc0cfaf082b
liuxu <liuxu@zqykj.com>
2023-11-11 17:27:07 +0800

    4.4 member模块修改启动日志,增加/member前缀，增加logback日志

--- member/src/main/java/com/study/train/member/MemberApplication.java
+++ member/src/main/java/com/study/train/member/MemberApplication.java
@@ -1,13 +1,22 @@
 package com.study.train.member;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class MemberApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(MemberApplication.class, args);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        String port = environment.getProperty("server.port");
+        LOGGER.info("项目启动成功，port:{}",port);
     }
 }
