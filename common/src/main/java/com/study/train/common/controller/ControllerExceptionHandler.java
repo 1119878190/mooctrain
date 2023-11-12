1d0742c63c981aa1081f7c224060772fbc29be59
liuxu <liuxu@zqykj.com>
2023-11-12 12:36:43 +0800

    4.17 为项目增加统一异常处理

---
+++ common/src/main/java/com/study/train/common/controller/ControllerExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.study.train.common.controller;
+
+
+import com.study.train.common.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 统一异常处理
+ */
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+
+    @ExceptionHandler(Exception.class)
+    public CommonResp exceptionHandler(Exception e) {
+        CommonResp<Object> resp = new CommonResp<>();
+        LOGGER.error("系统异常:" + e);
+        resp.setSuccess(false);
+        resp.setMessage(e.getMessage());
+        return resp;
+    }
+}
