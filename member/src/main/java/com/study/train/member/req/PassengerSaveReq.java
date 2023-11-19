c3f03cc4355075d804dafa626b00213b1a61efe2
liuxu <liuxu@zqykj.com>
2023-11-19 12:44:21 +0800

    7.3 乘车人保存接口字段非空校验

--- member/src/main/java/com/study/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/study/train/member/req/PassengerSaveReq.java
@@ -1,8 +1,12 @@
 package com.study.train.member.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.Date;
 
 public class PassengerSaveReq {
+
 
     /**
      * id
@@ -17,26 +21,31 @@ public class PassengerSaveReq {
     /**
      * 姓名
      */
+    @NotBlank(message = "【姓名】不能为空")
     private String name;
 
     /**
      * 身份证
      */
+    @NotBlank(message = "【身份证】不能为空")
     private String idCard;
 
     /**
      * 旅客类型|枚举[PassengerTypeEnum]
      */
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
     /**
      * 新增时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     /**
      * 修改时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
