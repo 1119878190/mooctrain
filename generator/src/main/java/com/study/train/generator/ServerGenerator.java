b8b3df2722609dba747cb94fbe2c736066d9d187
liuxu <liuxu@zqykj.com>
2023-11-22 22:54:50 +0800

    8.4 代码生成器---集成dom4j,读取pom.xml ,得到当前持久层的xml文件

---
+++ generator/src/main/java/com/study/train/generator/ServerGenerator.java
@@ -0,0 +1,28 @@
+package com.study.train.generator;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerGenerator {
+
+    static String pomPath = "generator/pom.xml";
+
+    public static void main(String[] args) throws DocumentException {
+
+        // 读取pom
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+
+
+    }
+}
