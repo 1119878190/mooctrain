8ae261310216e820f448a64cfcabd43e9023b793
liuxu <liuxu@zqykj.com>
2023-11-25 19:14:10 +0800

    9.3 去掉没用的HellowWorld组件，修改log区域

--- admin/src/views/main.vue
+++ admin/src/views/main.vue
@@ -26,21 +26,7 @@ export default defineComponent({
 
 
   setup() {
-    // const count = ref(0);
-    // axios.get("/member/member/count").then((response) => {
-    //   if(response.status === 200 && response.data.success){
-    //     let data = response.data
-    //     if (data.success) {
-    //       count.value = data.content
-    //     } else {
-    //       notification.error({description: data.message})
-    //     }
-    //   }
-    //
-    // })
-
     return{
-      // count
     }
 
   }
@@ -48,20 +34,4 @@ export default defineComponent({
 })
 </script>
 <style scoped>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
 </style>
