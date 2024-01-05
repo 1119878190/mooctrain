a96c7f9474d10264d9630702faae53338020bd10
liuxu <liuxu@zqykj.com>
2024-01-05 16:11:18 +0800

    11.9 增加生成每日车次功能

--- business/src/main/java/com/study/train/business/controller/admin/DailyTrainAdminController.java
+++ business/src/main/java/com/study/train/business/controller/admin/DailyTrainAdminController.java
@@ -9,7 +9,10 @@ import com.study.train.common.resp.CommonResp;
 import com.study.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/admin/daily-train")
@@ -38,6 +41,18 @@ public class DailyTrainAdminController {
     @DeleteMapping("/delete/{id}")
     public CommonResp<Object> delete(@PathVariable Long id) {
         dailyTrainService.delete(id);
+        return new CommonResp<>();
+    }
+
+
+    /**
+     * 生成每日的业务数据
+     * @param date
+     * @return
+     */
+    @GetMapping("/gen-daily/{date}")
+    public CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        dailyTrainService.genDaily(date);
         return new CommonResp<>();
     }
 
