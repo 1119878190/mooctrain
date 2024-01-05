beb070fef85305b225777c81430cff7417f00621
liuxu <liuxu@zqykj.com>
2024-01-05 15:01:00 +0800

    11.8 集成openfeign实现组件调用

--- batch/src/main/java/com/study/train/batch/BatchApplication.java
+++ batch/src/main/java/com/study/train/batch/BatchApplication.java
@@ -3,11 +3,13 @@ package com.study.train.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@MapperScan("com.study.train.*.mapper")
 @ComponentScan("com.study.train")
+@EnableFeignClients("com.study.train.batch.feign")
 public class BatchApplication {
 
     public static void main(String[] args) {
