8e791c84ba3a81f3b66a67e69655c19aff01371c
lx <1119878190@qq.com>
2024-01-17 21:51:01 +0800

    12.22 订单提交--选座成功后批量扣减影响到的多个库存

--- business/src/main/java/com/study/train/business/service/AfterConfirmOrderService.java
+++ business/src/main/java/com/study/train/business/service/AfterConfirmOrderService.java
@@ -6,6 +6,7 @@ import com.study.train.business.domain.DailyTrainTicket;
 import com.study.train.business.enums.ConfirmOrderStatusEnum;
 import com.study.train.business.mapper.ConfirmOrderMapper;
 import com.study.train.business.mapper.DailyTrainSeatMapper;
+import com.study.train.business.mapper.cust.DailyTrainTicketMapperCust;
 import com.study.train.business.req.ConfirmOrderTicketReq;
 import com.study.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
@@ -27,7 +28,7 @@ public class AfterConfirmOrderService {
     private DailyTrainSeatMapper dailyTrainSeatMapper;
 
     @Resource
-    private ConfirmOrderMapper confirmOrderMapper;
+    private DailyTrainTicketMapperCust dailyTrainTicketMapperCust;
 
     /**
      * 选中座位后事务处理：
@@ -35,10 +36,12 @@ public class AfterConfirmOrderService {
      * 余票详情表修改余票；
      * 为会员增加购票记录
      * 更新确认订单为成功
+     *
+     * @param dailyTrainTicket 每日车次余票信息
+     * @param finalSeatList    最终选择的座位
      */
     @Transactional
-    public void afterDoConfirm( List<DailyTrainSeat> finalSeatList)  {
-        // LOG.info("seata全局事务ID: {}", RootContext.getXID());
+    public void afterDoConfirm(DailyTrainTicket dailyTrainTicket, List<DailyTrainSeat> finalSeatList) {
         for (int j = 0; j < finalSeatList.size(); j++) {
             DailyTrainSeat dailyTrainSeat = finalSeatList.get(j);
             DailyTrainSeat seatForUpdate = new DailyTrainSeat();
@@ -46,6 +49,57 @@ public class AfterConfirmOrderService {
             seatForUpdate.setSell(dailyTrainSeat.getSell());
             seatForUpdate.setUpdateTime(new Date());
             dailyTrainSeatMapper.updateByPrimaryKeySelective(seatForUpdate);
+
+
+            // 计算这个站卖出去后，影响了哪些站的余票库存
+            // 参照2-3节 如何保证不超卖、不少卖，还要能承受极高的并发 10:30左右
+            // 影响的库存：本次选座之前没卖过票的，和本次购买的区间有交集的区间
+            // 假设10个站，本次买4~7站
+            // 原售：001000001
+            // 购买：000011100
+            // 新售：001011101
+            // 影响：XXX11111X
+            // Integer startIndex = 4;
+            // Integer endIndex = 7;
+            // Integer minStartIndex = startIndex - 往前碰到的最后一个0;
+            // Integer maxStartIndex = endIndex - 1;
+            // Integer minEndIndex = startIndex + 1;
+            // Integer maxEndIndex = endIndex + 往后碰到的最后一个0;
+            Integer startIndex = dailyTrainTicket.getStartIndex();
+            Integer endIndex = dailyTrainTicket.getEndIndex();
+            char[] chars = seatForUpdate.getSell().toCharArray();
+            Integer maxStartIndex = endIndex - 1;
+            Integer minEndIndex = startIndex + 1;
+            Integer minStartIndex = 0;
+            for (int i = startIndex - 1; i >= 0; i--) {
+                char aChar = chars[i];
+                if (aChar == '1') {
+                    minStartIndex = i + 1;
+                    break;
+                }
+            }
+            LOG.info("影响出发站区间：" + minStartIndex + "-" + maxStartIndex);
+
+            Integer maxEndIndex = seatForUpdate.getSell().length();
+            for (int i = endIndex; i < seatForUpdate.getSell().length(); i++) {
+                char aChar = chars[i];
+                if (aChar == '1') {
+                    maxEndIndex = i;
+                    break;
+                }
+            }
+            LOG.info("影响到达站区间：" + minEndIndex + "-" + maxEndIndex);
+
+            dailyTrainTicketMapperCust.updateCountBySell(
+                    dailyTrainSeat.getDate(),
+                    dailyTrainSeat.getTrainCode(),
+                    dailyTrainSeat.getSeatType(),
+                    minStartIndex,
+                    maxStartIndex,
+                    minEndIndex,
+                    maxEndIndex);
+
+
         }
     }
 }
