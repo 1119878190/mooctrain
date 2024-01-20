e211fe618cce0a9c14ab129598f3eff4cde651c2
lx <1119878190@qq.com>
2024-01-20 23:31:13 +0800

    13.4 集成nacos配置中心并简单使用

--- member/src/main/java/com/study/train/member/controller/TestController.java
+++ member/src/main/java/com/study/train/member/controller/TestController.java
@@ -1,15 +1,28 @@
 package com.study.train.member.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //@RefreshScope作用：在nacos修改配置，服务不用重启也生效
 public class TestController {
+
+    @Value("${pro}")
+    private String nacosTest;
 
     @GetMapping("/test")
     public String test() {
         return "hello world";
+    }
+
+
+    @GetMapping("/nacosTest")
+    public String nacosTest() {
+        System.out.println(nacosTest);
+        return nacosTest;
     }
 
 }
